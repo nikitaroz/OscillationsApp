1477b41f7ac7d1d365f3dca80202cd5af2e0cd66
nikitaroz <rozanovn@oregonstate.edu>
2016-11-27 12:44:43 -0800

    working on standardizing coding style

--- R/frequencyresult.R
+++ R/frequencyresult.R
@@ -10,22 +10,23 @@
 #' @export
 #'
 #' @examples
-setClass("FrequencyResult", representation(
-  data = "array",
-  frequency = "vector",
-  wavenumber = "vector"
-))
+setClass("FrequencyResult", 
+  representation(
+    data = "array", 
+    frequency = "vector", 
+    wavenumber = "vector"
+  )
+)
 
 
 setMethod("as.data.frame", signature("FrequencyResult"), 
-          function(x, row.names = NULL, optional = FALSE, ...) {
-            data <- as.vector(x@data)
-            nwavenumber <- length(x@wavenumber)
-            nfrequency <- length(x@frequency)
-            wavenumber <- rep(x@wavenumber, each = nfrequency)
-            frequency <- rep(x@frequency, times = nwavenumber)
-            df <- data.frame(wavenumber, frequency, data)
-            colnames(df) <- c("wavenumber", "frequency", "fft")
-            return(df)
-          }
-)
+  function(x, row.names = NULL, optional = FALSE, ...) {
+    data <- as.vector(x@data)
+    nwavenumber <- length(x@wavenumber)
+    nfrequency <- length(x@frequency)
+    wavenumber <- rep(x@wavenumber, each = nfrequency)
+    frequency <- rep(x@frequency, times = nwavenumber)
+    df <- data.frame(wavenumber, frequency, data)
+    colnames(df) <- c("wavenumber", "frequency", "fft")
+    return(df)
+})
--- R/src.R
+++ R/src.R
@@ -4,7 +4,7 @@ library(signal)
 library(Rwave)
 library(fields)
 library(doMC)
-registerDoMC(cores=4)
+registerDoMC(cores = 4)
 
 
 #' Title
@@ -16,9 +16,7 @@ registerDoMC(cores=4)
 #' @export
 #'
 #' @examples
-setGeneric("dft", function(object, window = NA)
-  standardGeneric("dft")
-)
+setGeneric("dft", function(object, window = NA) standardGeneric("dft"))
 
 
 #' Title
@@ -33,7 +31,7 @@ setGeneric("dft", function(object, window = NA)
 #' @export
 #'
 #' @examples
-setGeneric("wavelet", function(object, noctave = 2, nvoice = 8, fc = 1, ...)
+setGeneric("wavelet", function(object, noctave = 2, nvoice = 8, fc = 1, ...) 
   standardGeneric("wavelet")
 )
 
@@ -47,10 +45,11 @@ setGeneric("wavelet", function(object, noctave = 2, nvoice = 8, fc = 1, ...)
 #' @export
 #'
 #' @examples
-setGeneric("lomb", function(object, from = NULL, to = NULL)
+setGeneric("lomb", function(object, from = NULL, to = NULL) 
   standardGeneric("lomb")
 )
 
+# TOOD: This is a terrible API (change to plot?)
 #' Title
 #'
 #' @param x 
@@ -67,8 +66,10 @@ setGeneric("lomb", function(object, from = NULL, to = NULL)
 #' @examples
 setGeneric("imshow", function(x = seq(0, 1, length.out = nrow(z)), 
                               y = seq(0, 1, length.out = ncol(z)), 
-                              z, type = "contour", 
-                              interactive = FALSE, component = NA, ...)
+                              z, 
+                              type = "contour", 
+                              interactive = FALSE, 
+                              component = NA, ...) 
   standardGeneric("imshow")
 )
 
@@ -90,29 +91,28 @@ loadData <- function(file = file.choose(), sep = ",", ...) {
   spec.data@wavenumber <- as.vector(t(data[1, -1]))
   spec.data@data <- as.matrix(data)[-1, -1]
   spec.data@is.uniform <- FALSE
-  if(!is.unsorted(spec.data@wavenumber)){
-    
-  } else if(!is.unsorted(rev(spec.data@wavenumber))){
-    spec.data@wavenumber <- rev(spec.data@wavenumber)
-    spec.data@data <- t(apply(spec.data@data, 1, rev))
-  }
-  else {
-    stop("Dataset wavelengths are not ordered")
+  if (!is.unsorted(spec.data@wavenumber)) {
+      
+  } else if (!is.unsorted(rev(spec.data@wavenumber))) {
+      spec.data@wavenumber <- rev(spec.data@wavenumber)
+      spec.data@data <- t(apply(spec.data@data, 1, rev))
+  } else {
+      stop("Dataset wavelengths are not ordered")
   }
   bad.wavenumbers <- vector()
   sapply(seq(from = 1, to = length(spec.data@wavenumber), by = 1), 
-         function(i) {
-           if(all(spec.data@data[ , i] == 0)){
-             bad.wavenumbers <<- c(bad.wavenumbers, -i)
-           }
-         }
+    function(i) {
+      if (all(spec.data@data[, i] == 0)) {
+        bad.wavenumbers <<- c(bad.wavenumbers, -i)
+      }
+    }
   )
-  if(length(bad.wavenumbers) != 0) {
-  spec.data@wavenumber <- spec.data@wavenumber[bad.wavenumbers]
-  spec.data@data <- spec.data@data[, bad.wavenumbers]
+  
+  if (length(bad.wavenumbers) != 0) {
+    spec.data@wavenumber <- spec.data@wavenumber[bad.wavenumbers]
+    spec.data@data <- spec.data@data[, bad.wavenumbers]
   }
   colnames(spec.data@data) <- NULL
-  
   return(spec.data)
 }
 
@@ -129,36 +129,37 @@ loadData <- function(file = file.choose(), sep = ",", ...) {
 #' @export
 #'
 #' @examples
-trimData <- function(object, remove.negative = TRUE, 
+trimData <- function(object, 
+                     remove.negative = TRUE, 
                      uniform = FALSE, 
                      start.time = NA, 
-                     end.time = NA){
-  
+                     end.time = NA) {
+    
   # handles NA values for start.time
-  if(is.na(start.time)) {
-    # if remove.negative is set
-    if(remove.negative) {
-      start.time <- head(object@time[object@time >= 0], n = 1)
-    } else {
-      start.time <- head(object@time, n = 1)
+  if (is.na(start.time)) {
+      # if remove.negative is set
+      if (remove.negative) {
+        start.time <- head(object@time[object@time >= 0], n = 1)
+      } else {
+        start.time <- head(object@time, n = 1)
     }
   }
   # NA value for end.time
-  if(is.na(end.time)) {
+  if (is.na(end.time)) {
     end.time <- tail(object@time, n = 1)
   }
   # times to indicies
   start.idx <- which.min(abs(object@time - start.time))
   end.idx <- which.min(abs(object@time - end.time))
   # if the data is supposed to be uniform
-  if(uniform) {
+  if (uniform) {
     delta <- abs(object@time[start.idx + 1] - object@time[start.idx])
-    
-    for(i in (start.idx + 1):end.idx) {
+      
+    for (i in (start.idx + 1):end.idx) {
       difference <- abs(object@time[i - 1] - object@time[i])
-      if(abs(difference - delta) > 1E-5) {
+      if (abs(difference - delta) > 1e-05) {
         end.idx <- i - 1
-        break()
+        (break)()
       }
     }
   }
@@ -167,7 +168,7 @@ trimData <- function(object, remove.negative = TRUE,
   trimmed@time <- object@time[start.idx:end.idx]
   trimmed@wavenumber <- object@wavenumber
   trimmed@data <- object@data[start.idx:end.idx, ]
-  if(uniform) {
+  if (uniform) {
     trimmed@is.uniform <- TRUE
   } else {
     trimmed@is.uniform <- FALSE
@@ -188,8 +189,8 @@ trimData <- function(object, remove.negative = TRUE,
 #' @export
 #'
 #' @examples
-scaleData <- function(x, y, z, nx = nrow(z), ny = ncol(z)){
-  if(nx == nrow(z) & ny == ncol(z)){
+scaleData <- function(x, y, z, nx = nrow(z), ny = ncol(z)) {
+  if (nx == nrow(z) & ny == ncol(z)) {
     return(list(x = x, y = y, z = z))
   }
   data <- list(x = x, y = y, z = z)
@@ -202,34 +203,41 @@ scaleData <- function(x, y, z, nx = nrow(z), ny = ncol(z)){
 }
 
 
-setMethod("imshow", signature(z = "matrix"),
-          function(x = seq(0, 1, length.out = nrow(z)), 
-                   y = seq(0, 1, length.out = ncol(z)), 
-                   z, type = "contour", interactive = FALSE, 
-                   component = NA, ...){
-            
-            if(is.na(component)) {
-              
-            } else if(component == "intensity"){
-              z <- Mod(z)^2
-            } else if (component == "phase") {
-              z <- Arg(z)
-            }
-            
-            if(type == "contour" & !interactive){
-              return(filled.contour(x = x, y = y, z = z, 
-                                    color.palette = colorRamps::matlab.like, ...))
-            } else if(type == "contour" & interactive){
-              return(plot_ly(x = x, y = y, z = z, type = "contour", 
-                             line = list(width = 0), 
-                             colors = colorRamps::matlab.like(8), ...))
-            } else if(type == "surface" & interactive){
-              return(plot_ly(x = x, y = y, z = z, type = "surface", 
-                             colors = colorRamps::matlab.like(8), ...))
-            }
-          }
+setMethod("imshow", signature(z = "matrix"), 
+  function(x = seq(0, 1, length.out = nrow(z)), 
+           y = seq(0, 1, length.out = ncol(z)), 
+           z, type = "contour", interactive = FALSE, component = NA, ...) {
+    
+    if (is.na(component)) {
+        
+    } else if (component == "intensity") {
+      z <- Mod(z)^2
+    } else if (component == "phase") {
+      z <- Arg(z)
+    }
+    
+    if (type == "contour" & !interactive) {
+      return(
+        filled.contour(x = x, y = y, z = z, 
+                       color.palette = colorRamps::matlab.like, 
+                      ...)
+      )
+    } else if (type == "contour" & interactive) {
+      return(
+        plot_ly(x = x, y = y, z = z, 
+                type = "contour", 
+                line = list(width = 0), 
+                colors = colorRamps::matlab.like(8), ...)
+      )
+    } else if (type == "surface" & interactive) {
+      return(
+        plot_ly(x = x, y = y, z = z, 
+                type = "surface", 
+                colors = colorRamps::matlab.like(8), ...)
+      )
+    }
+  }
 )
 
-# Desired plot types:
-# interactive and static
-# contour, surface, and image
+# Desired plot types: interactive and static contour, surface, and
+# image
