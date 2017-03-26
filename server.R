3c70eda967f1461503424c3a93d4b2f5bbdb21e1
nikitaroz <rozanovn@oregonstate.edu>
2017-03-26 00:13:47 -0700

    cosmetic changes

--- server.R
+++ server.R
@@ -1,5 +1,3 @@
-
-
 library(shiny)
 library(RColorBrewer)
 library(plotrix)
--- ui.R
+++ ui.R
@@ -1,10 +1,4 @@
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 shinyUI(
   navbarPage(title = "Oscillations in Spectroscopy",
     tabPanel("Data Input", 
