a7953cdc8a7436b65a8cd31a3335d05b471e5560
nikitaroz <rozanovn@oregonstate.edu>
2017-03-26 00:46:01 -0700

    better wavelet frequency bounds

--- ui.R
+++ ui.R
@@ -81,7 +81,7 @@ shinyUI(
               sliderInput("noctaves", label = "octaves", 1, 10, value = 4),
               sliderInput("nvoices", label = "voices", 1, 16, value = 8),
               sliderInput("w0", label = "central frequency (ratio of 2\U03C0)", 
-                          0.5, 5.0, value = 1.0, step = 0.1)
+                          0.5, 2.0, value = 1.0, step = 0.1)
             ),
             column(9,
               plotOutput("wavelet"),
