88d6b6161b3b099ef059a95f24c76456f9dfef61
nikitaroz <rozanovn@oregonstate.edu>
2017-07-23 21:36:38 -0400

    Revert "stray commit"
    This reverts commit 2e7ea82c9beddef6be6da671d616bac89a6ae472.

--- server.R
+++ server.R
@@ -954,7 +954,16 @@ shinyServer(function(input, output, session) {
     }
     
     par(mar=c(1.5,5,1,7.5))
-    integrated.data <- colSums(data, label.spc = "integrated intensity")
+    if(input$fft.projection == "max"){
+      integrated.data <- apply(data, 2, max)
+      integrated.data@label$spc = "Max Intensity"
+      
+    }
+    else if(input$fft.projection == "integrated"){
+      integrated.data <- colSums(data, label.spc = "Integrated Intensity")
+    } else {
+      return(NULL)
+    }
     plotspc(integrated.data, plot.args = list(xaxs="i"),
             title.args = list(xlab = ""), 
             axis.args = list(x = list(labels = FALSE)), nxticks = nxticks)
@@ -1062,9 +1071,5 @@ shinyServer(function(input, output, session) {
     
     plotspc(integrated.data, plot.args = list(xaxs="i"))
   })
-<<<<<<< HEAD
 })
 >>>>>>> parent of b23c421... Update server.R
-=======
-})
->>>>>>> parent of 205119c... Update server.R
